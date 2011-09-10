3ef0120e1f0abb9ab7d4e0907e862e5b38a5202e
Michael Sievers <michael_sievers@web.de>
2011-09-10 18:45:30 +0200

    Make the clientHooks.js of the headings plugin contain something usefull.

--- plugins/headings/clientHooks.js
+++ plugins/headings/clientHooks.js
@@ -1,3 +1,47 @@
-exports.kafoo = function() {
-  alert('hej');
+exports.aceAttribsToClasses = function(args) {
+
+  if (args.key == 'heading1' && args.value != "")
+    return ["headings:h1"];
+
+  if (args.key == 'heading2' && args.value != "")
+    return ["headings:h2"];
+
+  if (args.key == 'heading3' && args.value != "")
+    return ["headings:h3"];
+
+  if (args.key == 'heading4' && args.value != "")
+    return ["headings:h4"];
+
+  if (args.key == 'heading5' && args.value != "")
+    return ["headings:h5"];
+
+  if (args.key == 'heading6' && args.value != "")
+    return ["headings:h6"];
+}
+
+exports.aceCreateDomLine = function(args) {
+
+  if (args.cls.indexOf('headings:') >= 0) {
+    cls = args.cls.replace(/(^| )headings:(\S+)/g, function(x0, space, typOfHeading) { return space + typOfHeading; });
+    return [{cls: cls, extraOpenTags: '', extraCloseTags: ''}];
+  }
+}
+
+exports.collectContentPre = function(args) {
+
+  // TODO: Speed the whole function up. It runs every time a character is typed.
+  var attributesToApply = [];
+
+  if(args.cls == 'ace-line') return;
+
+  if(args.cls.indexOf('h1') >= 0) attributesToApply.push('heading1');
+  else if(args.cls.indexOf('h2') >= 0) attributesToApply.push('heading2');
+  else if(args.cls.indexOf('h3') >= 0) attributesToApply.push('heading3');
+  else if(args.cls.indexOf('h4') >= 0) attributesToApply.push('heading4');
+  else if(args.cls.indexOf('h5') >= 0) attributesToApply.push('heading5');
+  else if(args.cls.indexOf('h6') >= 0) attributesToApply.push('heading6');
+
+  while(attributesToApply.length > 0) {
+    args.cc.doAttrib(args.state, attributesToApply.pop());
+  }
 }
