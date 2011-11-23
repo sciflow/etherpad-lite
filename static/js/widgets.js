599b9a947ceb99cc2fbd28d5808c213b144b374d
Michael Sievers <michael_sievers@web.de>
2011-11-23 02:14:59 +0100

    Added change handler for bibliography and meta informations.

--- static/js/widgets.js
+++ static/js/widgets.js
@@ -62,7 +62,7 @@ $(document).ready(function()
   updateUiWidgets();
 
   //poll for changes every 5 seconds
-  window.setInterval("updateUiWidgets()", 5000);
+  //window.setInterval("updateUiWidgets()", 5000);
 });
 
 function updateUiWidgets()
@@ -176,10 +176,30 @@ function updateUiWidgets()
 
       for(element in receivedData[collection])
       {
-        if(typeof(receivedData[collection][element]['data']) !== 'undefined' && typeof(receivedData[collection][element]['data']['title']) !== 'undefined')
+        var listText;
+
+        //we need to handle elements of the different collections differently
+        if(collection === 'metaInformations')
         {
-        $('#' + collection + 'List').append('<li id="' + receivedData[collection][element]['id']  + '" style="border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-         width:     0px; border-style: initial; border-color: initial; " class="ui-widget-content ui-selectee">' + receivedData[collection][element]['data']['title'] + '</li>');
+          if(typeof(receivedData[collection][element]['data']) === 'object' && typeof(receivedData[collection][element]['data']['metaInfoType']) === 'string')
+            listText = receivedData[collection][element]['data']['metaInfoType'];
         }
+        else if(collection === 'bibliography')
+        {
+          if(typeof(receivedData[collection][element]['data']) === 'object' && typeof(receivedData[collection][element]['data']['title']) === 'string')
+            listText = receivedData[collection][element]['data']['title'];
+        }
+        else if(collection === 'graphics')
+        {
+          if(typeof(receivedData[collection][element]['data']) === 'object' && typeof(receivedData[collection][element]['data']['title']) === 'string')
+            listText = receivedData[collection][element]['data']['title'];
+        }
+        else
+        {
+          listText = 'unknown element';
+        }
+
+        $('#' + collection + 'List').append('<li id="' + receivedData[collection][element]['id']  + '" style="border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-         width:     0px; border-style: initial; border-color: initial; " class="ui-widget-content ui-selectee">' + listText + '</li>');
       }
 
       //restore selected element
@@ -318,20 +338,72 @@ function handleUserInterfaceEvent(event)
     
     var addButtonHandler = function()
     {
-      var imageTitle = $(this).find('input[name="title"]').val();
-      var imageUrl = $(this).find('input[name="url"]').val();
+      var dialog = $(this);
+      var metaInfoType = dialog.find('select[name="metaInfoType"]').selectmenu("value");
 
-      
-      var addedGraphic = $('#graphicsList').prepend('\
-      <li class="ui-widget-content">\
-        <div class="ui-widget-header" >' + imageTitle + '</div>\
-        <img src="' + imageUrl + '" height="96" //>\
-        </li>\
-      ');
-      
-      //$(addedGraphic).find('li').show('highlight', 'slow', setTimeout($(this).dialog("close"),1));
-      $(this).dialog("close");
+      //check if somebody trys to add something which can only exist one time
+      if(metaInfoType === 'Categories' && $('#metaInformationsList li:contains("Categories")').length > 0)
+      {
+        alert("There can only be one categories entry. Please use the modify option.");
+        dialog.dialog("close");
+        return;
+      }
+      else if(metaInfoType === 'General Terms' && $('#metaInformationsList li:contains("General Terms")').length > 0)
+      {
+        alert("There can only be one general terms entry. Please use the modify option.");
+        dialog.dialog("close");
+        return;
+      }
+      else if(metaInfoType === 'Keywords' && $('#metaInformationsList li:contains("Keywords")').length > 0)
+      {
+        alert("There can only be one keywords entry. Please use the modify option.");
+        dialog.dialog("close");
+        return;
+      }
+      else if(metaInfoType === 'Subtitle' && $('#metaInformationsList li:contains("Subtitle")').length > 0)
+      {
+        alert("There can only be one subtitle entry. Please use the modify option.");
+        dialog.dialog("close");
+        return;
+      }
+      else if(metaInfoType === 'Title' && $('#metaInformationsList li:contains("Title")').length > 0)
+      {
+        alert("There can only be one title entry. Please use the modify option.");
+        dialog.dialog("close");
+        return;
+      }
 
+      //build the parameter object used when calling the api
+      var requestParameter = {
+        metaInfoType : metaInfoType,
+        name : (metaInfoType === 'Author') ? dialog.find('*[name="name"]').val() : undefined,
+        position : (metaInfoType === 'Author') ? dialog.find('*[name="position"]').val() : undefined,
+        organization : (metaInfoType === 'Author') ? dialog.find('*[name="organization"]').val() : undefined,
+        telephone : (metaInfoType === 'Author') ? dialog.find('*[name="telephone"]').val() : undefined,
+        email : (metaInfoType === 'Author') ? dialog.find('*[name="email"]').val() : undefined,
+        position : (metaInfoType === 'Author') ? dialog.find('*[name="position"]').val() : undefined,
+        adress : (metaInfoType === 'Author') ? dialog.find('*[name="adress"]').val() : undefined,
+        categories : (metaInfoType === 'Categories') ? dialog.find('*[name="categories"]').val() : undefined,
+        generalTerms : (metaInfoType === 'General Terms') ? dialog.find('*[name="generalTerms"]').val() : undefined,
+        keywords : (metaInfoType === 'Keywords') ? dialog.find('*[name="keywords"]').val() : undefined,
+        subtitle : (metaInfoType === 'Subtitle') ? dialog.find('*[name="subtitle"]').val() : undefined,
+        title : (metaInfoType === 'Title') ? dialog.find('*[name="title"]').val() : undefined
+      };
+
+      $.ajax({
+        url : '/api/2/pads/' + location.href.match(/p\/([0-9a-zA-Z_]+)$/)[1] + '/datastores/metaInformations',
+        type : 'POST',
+        dataType: 'json',
+        async : false,
+        processData: false,
+        contentType : 'application/json',
+        data : JSON.stringify(requestParameter)
+      });
+
+      //TODO: replace with updating the bibliography list
+      updateUiWidgets();
+
+      dialog.dialog("close");
     }
     
     var cancelButtonHandler = function()
@@ -430,7 +502,34 @@ function handleUserInterfaceEvent(event)
     
     $(dialog).dialog('open');
   }
+  //
+  // Delete bibliography
+  //
+  else if(event.origin === 'metaInformationsCommandBar.DeleteButton')
+  {
+    var item;
 
+    //get the element which should be deleted
+    var itemsToDelete = $('#metaInformationsList li.ui-selected');
+
+    if(itemsToDelete.length === 0)
+    {
+      //there are no items selected
+      return;
+    }
+
+    for(item in itemsToDelete)
+    {
+      //try to delete the item using delete api calls
+      $.ajax({
+        url : '/api/2/pads/' + location.href.match(/p\/([0-9a-zA-Z_]+)$/)[1] + '/datastores/metaInformations/' + $(itemsToDelete[item]).attr('id'),
+        type : 'DELETE',
+        async: false,
+      });
+
+      $(itemsToDelete[item]).remove();
+    }
+  }
   //
   // Add bibliography
   //
@@ -444,7 +543,7 @@ function handleUserInterfaceEvent(event)
 
       //build the parameter object used when calling the api
       var requestParameter = {
-        bibliographyType : dialog.find('select[name="entryType"]').val(),
+        entryType : dialog.find('select[name="entryType"]').val(),
         title : dialog.find('input[name="title"]').val(),
         authors : dialog.find('input[name="authors"]').val(),
         url : dialog.find('input[name="url"]').val(),
@@ -471,7 +570,6 @@ function handleUserInterfaceEvent(event)
       updateUiWidgets();
 
       $(this).dialog("close");
-
     }
     
     var cancelButtonHandler = function()
@@ -520,9 +618,125 @@ function handleUserInterfaceEvent(event)
   else if(event.origin === 'bibliographyCommandBar.ChangeButton')
   {
     //get selected list element
-    
+    var selectedElementId = $('#bibliographyList .ui-selected').attr('id');
 
-    alert('Change bibliography');
+    if(typeof(selectedElementId) !== 'undefined')
+    {
+      var datastoreObject;
+
+      //get the object from the datastore
+      $.ajax(
+      {
+        url : '/api/2/pads/' + location.href.match(/p\/([0-9a-zA-Z_]+)$/)[1] + '/datastores/bibliography/' + selectedElementId,
+        dataType : 'json',
+        async : false,
+        success : function(result)
+        {
+          datastoreObject = result;
+        }
+      });
+
+      //if the result is something we can work with
+      if(typeof(datastoreObject) === 'object')
+      {
+        //define the changeButton handler
+        var changeButtonHandler = function()
+        {
+          var dialog = $(this);
+
+          //build the parameter object used when calling the api
+          var requestParameter = {
+            entryType : dialog.find('select[name="entryType"]').val(),
+            title : dialog.find('input[name="title"]').val(),
+            authors : dialog.find('input[name="authors"]').val(),
+            url : dialog.find('input[name="url"]').val(),
+            year : dialog.find('input[name="year"]').val(),
+            month : dialog.find('input[name="month"]').val(),
+            publisher : dialog.find('input[name="publisher"]').val(),
+            journal : dialog.find('input[name="journal"]').val()
+          };
+
+          $.ajax({
+            url : '/api/2/pads/' + location.href.match(/p\/([0-9a-zA-Z_]+)$/)[1] + '/datastores/bibliography/' + selectedElementId,
+            type : 'PUT',
+            dataType: 'json',
+            async : false,
+            processData: false,
+            contentType : 'application/json',
+            data : JSON.stringify(requestParameter)
+          });
+
+          //TODO: replace with updating the bibliography list
+          updateUiWidgets();
+
+          $(this).dialog("close");
+        }
+
+        //define the cancelButton handler
+        var cancelButtonHandler = function()
+        {
+          var dialog = $(this);
+          dialog.dialog("close");
+        }
+
+        //get the dialog
+        var dialogHtml = getDialogHtml('bibliography');
+
+        //create the selectMenu widget
+        dialogHtml.find('select').selectmenu(
+        {
+          width: '150px'
+        });
+
+        //style the selectMenu (dropdown button)
+        dialogHtml.find('#entryType-button').css({
+          'margin-top' : '5px',
+          'margin-bottom' : '5px'
+        });
+
+        //prefill the dialog with the data received by the api call to the datastore
+        var element;
+
+        for(element in datastoreObject)
+        {
+          if(element === 'entryType')
+          {
+            dialogHtml.find('select').selectmenu("value", (typeof(datastoreObject[element]) === 'string') ? datastoreObject[element] : '');
+          }
+          else
+          {
+            dialogHtml.find('input[name="' + element  +'"]').val((typeof(datastoreObject[element]) === 'string') ? datastoreObject[element] : '');
+          }
+        }
+        
+        var foo = 'bla';
+
+        //create the dialog
+        var dialog = $(dialogHtml).dialog({
+          autoOpen: false,
+          title: 'Change bibliography',
+          modal: true,
+          width: 400,
+          buttons: [
+            {
+              text: 'Change',
+              click: changeButtonHandler
+            },
+            {
+              text: 'Cancel',
+              click: cancelButtonHandler
+            }
+          ]
+        });
+
+        //show the dialog
+        $(dialog).dialog('open');
+      }
+    }
+    else
+    {
+      alert('No element selected!');
+    }
   }
 
   //
@@ -655,6 +869,12 @@ function handleUserInterfaceEvent(event)
         }
       ]
     });
-    
+  }
+  else if(event.origin === 'graphicsCommandBar.InsertButton')
+  {
+    var selectedImage = $('#graphicsList li.ui-selected').find('img');
+
+    //call the additional markup plugin
+    plugins.callHook('handleCommand', {name: 'addGraphic', parameters: { url: $(selectedImage).attr('src') } });
   }
 }
